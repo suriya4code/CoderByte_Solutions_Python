7970d9fc7e78916381e2505137bae4de6fb8df49
suriya4code <suriya4code@gmail.com>
2022-02-01 18:46:30 +0530

    AgeCounting

--- app_AgeCounting_above_50.py
+++ app_AgeCounting_above_50.py
@@ -1,1 +1,23 @@
-#init
+import requests
+
+url = "https://coderbyte.com/api/challenges/json/age-counting"
+
+data = requests.get(url).json()['data']
+
+'''Solution 1'''
+def get50plusAge():
+    count = 0
+    for item in data.split(','):
+        ds = item.split('=')
+        if ds[0].strip() == "age" and int(ds[1]) >= 50:
+            count += 1
+
+    return count
+
+'''Solution 2'''
+def get50plusAge2():
+    x = lambda a: a.split('=')[0].strip()=="age" and int(a.split('=')[1])>=50
+    return int(len(list(filter(x,data.split(',')))))
+
+print(get50plusAge())
+print(get50plusAge2())
